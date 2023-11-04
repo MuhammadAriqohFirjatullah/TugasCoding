d9c0c9edd68266b56a84cf190685c3e51b10ac57
MuhammadAriqohFirjatullah <149862480+MuhammadAriqohFirjatullah@users.noreply.github.com>
2023-11-04 16:21:28 +0700

    Add files via upload

---
+++ task.java/tugas4.java
@@ -0,0 +1,12 @@
+public class tugas4 {
+    public static void main(String[] args) {
+        String[] TempatWisataTujuansaya = {
+            "Bali", "Bandung", "Jepang", "Korea Selatan"
+        };
+        for (int i=0 ; i<TempatWisataTujuansaya.length; i++){
+            System.out.println(TempatWisataTujuansaya[i]);
+        }
+    }
+}
+
+
